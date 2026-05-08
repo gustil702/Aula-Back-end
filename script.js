11df7f6a49c2b0fdc54269056fdea51b23e956de
gustil702 <iohann.tanferre@escola.pr.gov.br>
2026-05-08 00:46:24 +0000

    ççç

---
+++ script.js
@@ -0,0 +1,7 @@
+const num1= 9;
+const num2= 6;
+
+console.log("soma:" + (num1+num2));
+console.log("subtração: " + (num1-num2));
+console.log("multiplicação: " + (num1*num2));
+console.log("divisão: " + (num1/num2));
